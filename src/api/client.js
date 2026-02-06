d9f6784ab5cf759414b10254ac8b4d9cb1135063
Saimun Hasan <saimunhasanrifat14@gmail.com>
2026-02-07 01:50:07 +0600

    add api interceptor

--- src/api/client.js
+++ src/api/client.js
@@ -1,10 +1,45 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || '';
+const baseURL = import.meta.env.VITE_API_URL || "";
 
 const api = axios.create({
   baseURL,
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    // If access token expired
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        // call refresh endpoint
+        await axios.post(`${baseURL}/api/v1/auth/refresh-token`, {}, { withCredentials: true });
+        return api(originalRequest);
+      } catch (err) {
+        console.log("Refresh failed. Logging out.");
+
+        if (window.location.pathname !== "/login") {
+          window.location.href = "/login";
+        }
+        return Promise.reject(err);
+      }
+    }
+
+    if (error.response?.status === 403) {
+      console.log("Session expired. Logging out.");
+      if (window.location.pathname !== "/login") {
+        window.location.href = "/login";
+      }
+      return Promise.reject(error);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 
 export default api;
